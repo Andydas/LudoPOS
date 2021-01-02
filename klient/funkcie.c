27292907a27d19be26bfceffee2ff883c6d05ec4
andydas <drzik.andrej@gmail.com>
2021-01-02 13:06:29 +0100

    klient asi funguje

--- klient/funkcie.c
+++ klient/funkcie.c
@@ -14,26 +14,10 @@ void komunikacia(void* param) {
     bzero(buffCitanie, 256);
     bzero(buffZapisovanie, 256);
 
-    //zistenie mojho ID od serveru
-    /*bzero(data->buffer,256);
-    read(data->sock, data->buffer, 256);
-    char* idecko = data->buffer;
-    data->ID = atoi(idecko);
-    printf("Moje ID je %d\n", data->ID);*/
-
 
 
     //posielanie sprav
     while (!koniec) {
-
-        /*//klient caka kym je na rade, az potom posiela spravy
-        while (data->somNaRade != 1){
-            bzero(data->buffer,256);
-            read(data->sock, data->buffer, 256);
-            char* somNaRade = data->buffer[3];
-            data->somNaRade = atoi(somNaRade);
-            //printf("Som na rade: %d", data->somNaRade);
-        }*/
 
         //citanie spravy od servera
         //bzero(buffCitanie, 256);
@@ -46,16 +30,30 @@ void komunikacia(void* param) {
         data->ID = buffCitanie[0];
         data->ktoJeNaRade = buffCitanie[4];
         printf("Od servera som zistil tieto informacie:\n");
-        printf("Moje id je: %d\n", data->ID);
+        printf("Moje ideeecko je: %d\n", data->ID);
         printf("Naposledy hral id: %d\n", buffCitanie[1]);
-        printf("Posuval panacika %d o %d policok.\n", buffCitanie[2], buffCitanie[3]);
+        printf("Posuval panacika %d o %d policok.\n", buffCitanie[3], buffCitanie[2]);
         printf("Teraz je na rade: %d\n", data->ktoJeNaRade);
-
 
         //kym nie som na rade tak cakam
         while (data->ID != data->ktoJeNaRade){
-            pthread_cond_wait(data->odosli, data->mut);
+            //citanie spravy od servera
+            //bzero(buffCitanie, 256);
+            int uspech = read(data->sock, buffCitanie, 255);
+            if (uspech < 0) {
+                perror("Chyba pri citani zo socketu\n");//kontrola co precitalo
+            }
+
+            //zapisem si co som precital
+            data->ID = buffCitanie[0];
+            data->ktoJeNaRade = buffCitanie[4];
+            printf("Od servera som zistil tieto informacie:\n");
+            printf("Moje id je: %d\n", data->ID);
+            printf("Naposledy hral id: %d\n", buffCitanie[1]);
+            printf("Posuval panacika %d o %d policok.\n", buffCitanie[2], buffCitanie[3]);
+            printf("Teraz je na rade: %d\n", data->ktoJeNaRade);
         }
+
         printf("Teraz si na rade, tvoj vstup bude odoslany na server.\n");
 
         //hadzem koockou a zapisujem to serveru
@@ -86,10 +84,19 @@ void komunikacia(void* param) {
                 }
                 printf("Uspesne zapisane do socketu!\n");
                 okVstup = true;
+            } else {
+                printf("Neplany vstup.\n");
             }
-            printf("Neplany vstup.\n");
         }
 
     }
 
+}
+
+void zapisuj(int id, int hod, int panacik) {
+
+}
+
+char *citaj() {
+    return NULL;
 }
